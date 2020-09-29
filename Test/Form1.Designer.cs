1d8ddbc12e3fc387fffa02bb38993513106384d6
ivicatasic <ivica.tasic@pmf.edu.rs>
2020-09-30 01:15:14 +0200

    Add Test

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(496, 340);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 173);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 164);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chose Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(251, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(242, 164);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox3, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox4, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox5, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox3, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBox4, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.textBox5, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.textBox6, 1, 5);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(251, 173);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.77778F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.4321F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.58025F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.19753F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(242, 164);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 40);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Show Folders";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(124, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 40);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Show Checksums";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "MD5";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(54, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "SHA1";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 101);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(66, 16);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "SHA256";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(3, 123);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(66, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "SHA384";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(3, 146);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(66, 15);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.Text = "SHA512";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(124, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(124, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(115, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox4.Location = new System.Drawing.Point(124, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(115, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox5.Location = new System.Drawing.Point(124, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(115, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox6.Location = new System.Drawing.Point(124, 146);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(115, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 340);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+
+        private string path;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            path = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+                if (result ==DialogResult.OK)
+                    textBox1.Text = dialog.SelectedPath;
+                     path= dialog.SelectedPath;
+                string[] files = Directory.GetFiles(dialog.SelectedPath);
+            }
+        }
+
+        
+        
+        //create treeView with selected folder path
+        public void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+
+        }
+
+        //create node in treeView
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+
+            foreach(var directory in directoryInfo.GetDirectories())
+            {
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            }
+            foreach(var file in directoryInfo.GetFiles())
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            }
+
+            return directoryNode;
+        }
+
+        //click on show folders
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                ListDirectory(treeView1, textBox1.Text.ToString());
+            }
+        }
+
+        //checksum with md5
+        public static string CreateDirectoryMd5(string srcPath)
+        {
+            var filePaths = Directory.GetFiles(srcPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+
+            using (var md5 = MD5.Create())
+            {
+                foreach (var filePath in filePaths)
+                {
+                    // hash path
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    md5.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    byte[] contentBytes = File.ReadAllBytes(filePath);
+
+                    md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                }
+
+                //Handles empty filePaths case
+                md5.TransformFinalBlock(new byte[0], 0, 0);
+
+                return BitConverter.ToString(md5.Hash).Replace("-", "").ToLower();
+            }
+        }
+
+        //checksum with sha1 
+        public static string CreateDirectorySha1(string srcPath)
+        {
+            var filePaths = Directory.GetFiles(srcPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+
+            using (var sha1 = SHA1.Create())
+            {
+                foreach (var filePath in filePaths)
+                {
+                    // hash path
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    sha1.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    byte[] contentBytes = File.ReadAllBytes(filePath);
+
+                    sha1.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                }
+
+                //Handles empty filePaths case
+                sha1.TransformFinalBlock(new byte[0], 0, 0);
+
+                return BitConverter.ToString(sha1.Hash).Replace("-", "").ToLower();
+            }
+        }
+
+        //checksum with sha256
+        public static string CreateDirectorySha256(string srcPath)
+        {
+            var filePaths = Directory.GetFiles(srcPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+
+            using (var sha256 = SHA256.Create())
+            {
+                foreach (var filePath in filePaths)
+                {
+                    // hash path
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    sha256.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    byte[] contentBytes = File.ReadAllBytes(filePath);
+
+                    sha256.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                }
+
+                //Handles empty filePaths case
+                sha256.TransformFinalBlock(new byte[0], 0, 0);
+
+                return BitConverter.ToString(sha256.Hash).Replace("-", "").ToLower();
+            }
+        }
+
+        //checksum with sha384
+        public static string CreateDirectorySha384(string srcPath)
+        {
+            var filePaths = Directory.GetFiles(srcPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+
+            using (var sha384 = SHA384.Create())
+            {
+                foreach (var filePath in filePaths)
+                {
+                    // hash path
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    sha384.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    byte[] contentBytes = File.ReadAllBytes(filePath);
+
+                    sha384.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                }
+
+                //Handles empty filePaths case
+                sha384.TransformFinalBlock(new byte[0], 0, 0);
+
+                return BitConverter.ToString(sha384.Hash).Replace("-", "").ToLower();
+            }
+        }
+
+        //checksum with sha512
+        public static string CreateDirectorySha512(string srcPath)
+        {
+            var filePaths = Directory.GetFiles(srcPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+
+            using (var sha512 = SHA512.Create())
+            {
+                foreach (var filePath in filePaths)
+                {
+                    // hash path
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    sha512.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                    // hash contents
+                    byte[] contentBytes = File.ReadAllBytes(filePath);
+
+                    sha512.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                }
+
+                //Handles empty filePaths case
+                sha512.TransformFinalBlock(new byte[0], 0, 0);
+
+                return BitConverter.ToString(sha512.Hash).Replace("-", "").ToLower();
+            }
+        }
+
+        //show checksum on click(button:Show Checksums)
+        //write in textBoxes checksums for checked algorithm
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked)
+            {
+                string res = CreateDirectoryMd5(path);
+                textBox2.Text = res.ToString();
+            }
+            if (checkBox2.Checked)
+            {
+                string res = CreateDirectorySha1(path);
+                textBox3.Text = res.ToString();
+            }
+            if (checkBox3.Checked)
+            {
+                string res = CreateDirectorySha256(path);
+                textBox4.Text = res.ToString();
+            }
+            if (checkBox4.Checked)
+            {
+                string res = CreateDirectorySha384(path);
+                textBox5.Text = res.ToString();
+            }
+            if (checkBox5.Checked)
+            {
+                string res = CreateDirectorySha512(path);
+                textBox6.Text = res.ToString();
+            }
+        }
+
+
+        //click on treeView node(if file attribute exist get it, else throw excetion)
+        //on click in messagebox show information of file
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+            string fileName = "";
+           
+            
+
+            fileName = treeView1.SelectedNode.Text;
+            
+
+            
+            long sizeFile=0; 
+
+            
+            char[] getE = path.ToArray();
+            char oz = getE.ElementAt(2);
+            string res = path +oz+ treeView1.SelectedNode.Text;
+            FileInfo fileInfo2 = new FileInfo(res);
+
+            //if we check the file
+            if (File.Exists(res))
+            {
+                sizeFile = fileInfo2.Length;
+            }
+            else//folder and other
+            {
+                MessageBox.Show("File not found!");
+            }
+            
+            DateTime createdDate= File.GetCreationTime(res);
+
+            //if attributes exist
+            FileAttributes describe = File.GetAttributes(res);
+            
+           
+            MessageBox.Show("Name: "+fileName+ "\n" +"Size: " + sizeFile.ToString()+"\n"+"Date: "+createdDate.ToString()+"\n"+"Describe: "+describe.ToString());
+        }
+
+        
+    }
+}
